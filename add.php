0455dc2ae19ef992fc78e66f275c013cb82f4297
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2022-12-15 12:04:47 +1300

    POST method

--- add.php
+++ add.php
@@ -1,9 +1,9 @@
 <?php
 
-    if(isset($_GET['submit'])) {
-        echo $_GET['email'];
-        echo $_GET['title'];
-        echo $_GET['ingredients'];
+    if(isset($_POST['submit'])) {
+        echo $_POST['email'];
+        echo $_POST['title'];
+        echo $_POST['ingredients'];
     }
 
 
@@ -16,7 +16,7 @@
 
     <section class="container grey-text">
         <h4 class="center">Add a Pizza</h4>
-        <form action="add.php" class="white" method="GET">
+        <form action="add.php" class="white" method="POST">
             <label>Your Email:</label>
             <input type="text" name="email">
             <label>Pizza Title:</label>
