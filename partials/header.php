75fc88d37378f56e6f285e41b49f91eafbaaf927
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2022-12-15 11:53:17 +1300

    add form

--- partials/header.php
+++ partials/header.php
@@ -6,10 +6,15 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <style type="text/css">
 	    .brand{
-	  	background: #cbb09c !important;
+	  	    background: #cbb09c !important;
 	    }
   	    .brand-text{
-  		color: #cbb09c !important;
+  		    color: #cbb09c !important;
+  	    }
+        form{
+  		    max-width: 460px;
+  		    margin: 20px auto;
+  		    padding: 20px;
   	    }
   </style>
 </head>
