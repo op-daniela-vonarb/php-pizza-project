f2e05194390cbdbc48f3e2702c3b5153b4023caa
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2022-12-15 14:11:12 +1300

    add form validation

--- add.php
+++ add.php
@@ -5,17 +5,26 @@
         if(empty($_POST['email'])) {
             echo 'An email is required <br />';
         } else {
-            echo htmlspecialchars($_POST['email']);
+            $email = $_POST['email'];
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                echo 'Email must be a valid email address';
+            }
         }
         if(empty($_POST['title'])) {
             echo 'A title is required <br />';
         } else {
-            echo htmlspecialchars($_POST['title']);
+            $title = $_POST['title'];
+            if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+				echo 'Title must be letters and spaces only';
+			}
         }
         if(empty($_POST['ingredients'])) {
             echo 'At least one ingredient is required <br />';
         } else {
-            echo htmlspecialchars($_POST['ingredients']);
+            $ingredients = $_POST['ingredients'];
+			if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+				echo 'Ingredients must be a comma separated list';
+			}
         }
     }
 
