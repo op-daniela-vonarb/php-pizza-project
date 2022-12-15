efc32df7ea2674590da622fcfcdaf5e4d68b181d
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2022-12-16 09:14:55 +1300

    add delete button

--- details.php
+++ details.php
@@ -27,11 +27,18 @@
 
     <div class="container center">
 		<?php if($pizza): ?>
+
 			<h4><?php echo htmlspecialchars($pizza['title']); ?></h4>
 			<p>Created by <?php echo htmlspecialchars($pizza['email']); ?></p>
 			<p><?php echo ($pizza['created_at']); ?></p>
 			<h5>Ingredients:</h5>
 			<p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+
+            <form>
+                <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id'] ?>">
+                <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+            </form>
+
 		<?php else: ?>
 			<h5>No such pizza exists.</h5>
 		<?php endif ?>
