85ed3168e07510235e6eeda51d380b29c3a65754
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2022-12-15 15:28:47 +1300

    create db connection

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+    $conn = mysqli_connect('localhost', 'daniela', 'test1234', 'ninja_pizza');
+
+    if(!$conn) {
+        echo 'Connection error:' . mysqli_connect_error();
+    }
+
 
 ?>
 
