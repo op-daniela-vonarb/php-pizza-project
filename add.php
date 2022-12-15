202212bab70fd8beeffba6530309d1fa7298ad90
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2022-12-15 13:55:18 +1300

    add htmlspecialchars

--- add.php
+++ add.php
@@ -1,9 +1,22 @@
 <?php
 
     if(isset($_POST['submit'])) {
-        echo $_POST['email'];
-        echo $_POST['title'];
-        echo $_POST['ingredients'];
+
+        if(empty($_POST['email'])) {
+            echo 'An email is required <br />';
+        } else {
+            echo htmlspecialchars($_POST['email']);
+        }
+        if(empty($_POST['title'])) {
+            echo 'A title is required <br />';
+        } else {
+            echo htmlspecialchars($_POST['title']);
+        }
+        if(empty($_POST['ingredients'])) {
+            echo 'At least one ingredient is required <br />';
+        } else {
+            echo htmlspecialchars($_POST['ingredients']);
+        }
     }
 
 
