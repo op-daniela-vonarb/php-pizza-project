7b327e8689f6e5640f663907060a0157e4409280
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2022-12-17 20:29:17 +1300

    complete login functionality

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+    include_once 'partials/header.php';
+?>
+
+     
+    <div class="index-login-login">
+        <h4>LOGIN</h4>
+        <p>Don't have an account yet? Sign up here!</p>
+        <form action="includes/login.inc.php" method="post">
+            <input type="text" name="uid" placeholder="Username/Email...">
+            <input type="password" name="pwd" placeholder="Password">
+            <br>
+            <button type="submit" name="submit">LOGIN</button>
+        </form>
+        <?php
+        if(isset($_GET["error"])) {
+            if($_GET["error"] == "emptyinput") {
+                echo "<p>Fill in all fields!</p>";
+            }
+            else if ($_GET["error"] == "wronglogin") {
+                echo "<p>Incorrect login information</p>";
+            }
+        }
+    ?>        
+    </div>
+<?php
+    include_once 'partials/footer.php';
+?>
