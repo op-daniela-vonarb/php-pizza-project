6129f79abb67f12552993481915f0d0f8b3bb09f
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2022-12-18 14:44:38 +1300

    create SignupContr class

---
+++ classes/signup-contr.class.php
@@ -0,0 +1,102 @@
+<?php
+
+
+class SignupContr extends Signup {
+
+    private $name;
+    private $email;
+    private $username;
+    private $pwd;
+    private $pwdRepeat;
+
+    public function __construct($name, $email, $username, $pwd, $pwdRepeat) {
+        $this->name = $name;
+        $this->email = $email;
+        $this->username = $username;
+        $this->pwd = $pwd;
+        $this->pwdRepeat = $pwdRepeat;
+    }
+
+    public function signupUser() {
+        if($this->emptyInput() == false) {
+            // echo "Empty input!";
+            header("location: ../index.php?error=emptyinput");
+            exit();
+        }
+        if($this->invalidUid() == false) {
+            // echo "Invalid username!";
+            header("location: ../index.php?error=username");
+            exit();
+        }
+        if($this->invalidEmail() == false) {
+            // echo "invalid email!";
+            header("location: ../index.php?error=email");
+            exit();
+        }
+        if($this->pwdMatch() == false) {
+            // echo "passwords don't match!";
+            header("location: ../index.php?error=passwordmatch");
+            exit();
+        }
+        if($this->uidTakenCheck() == false) {
+            // echo "username or email taken";
+            header("location: ../index.php?error=useroremailtaken");
+            exit();
+        }
+
+        $this->setUser($this->name, $this->email, $this->username, $this->pwd);
+
+    }
+
+    private function emptyInput() {
+        $result = '';
+        if(empty($this->name) || empty($this->email) || empty($this->username) || empty($this->pwd) || empty($this->pwdRepeat)) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function invalidUid() {
+        $result = '';
+        if(!preg_match("/^[a-zA-Z0-9]*$/", $this->username)) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function invalidEmail() {
+        $result = '';
+        if(!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+        return $result;
+    }
+   
+    private function pwdMatch() {
+        $result = '';
+        if($this->pwd !== $this->pwdRepeat) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function uidTakenCheck() {
+        $result = '';
+        if(!$this->checkUser($this->username, $this->email)) {
+            $result = false;
+        }
+        else {
+            $result = true;
+        }
+        return $result;
+    }
+
+}
