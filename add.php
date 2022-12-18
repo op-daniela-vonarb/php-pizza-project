02eb35ed775077acda817c40dbfe6bd32741121f
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2022-12-18 13:18:26 +1300

    remove email in add pizza

--- add.php
+++ add.php
@@ -3,18 +3,10 @@
     include 'includes/dbh.inc.php';
 
     $title = $email = $ingredients = '';
-    $errors = array('email' => '', 'title' => '', 'ingredients' => '');
+    $errors = array('title' => '', 'ingredients' => '');
 
     if(isset($_POST['submit'])) {
 
-        if(empty($_POST['email'])) {
-            $errors['email'] = 'An email is required <br />';
-        } else {
-            $email = $_POST['email'];
-            if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $errors['email'] = 'Email must be a valid email address';
-            }
-        }
         if(empty($_POST['title'])) {
             $errors['title'] = 'A title is required <br />';
         } else {
@@ -35,11 +27,10 @@
         if(array_filter($errors)) {
             //echo 'errors in the form';
         } else {
-            $email = mysqli_real_escape_string($conn, $_POST['email']);
             $title = mysqli_real_escape_string($conn, $_POST['title']);
             $ingredients = mysqli_real_escape_string($conn, $_POST['ingredients']);
 
-            $sql = "INSERT INTO pizzas(title, email, ingredients) VALUES('$title', '$email', '$ingredients')";
+            $sql = "INSERT INTO pizzas(title, ingredients) VALUES('$title', '$ingredients')";
 
             if(mysqli_query($conn, $sql)) {
 
@@ -62,9 +53,6 @@
     <section class="container grey-text">
         <h4 class="center">Add a Pizza</h4>
         <form action="add.php" class="white" method="POST">
-            <label>Your Email:</label>
-            <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
-            <div class="red-text"><?php echo $errors['email']; ?></div>
             <label>Pizza Title:</label>
             <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
             <div class="red-text"><?php echo $errors['title']; ?></div>
