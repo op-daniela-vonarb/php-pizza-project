147c15963c8317de7dce527b13da4620cca0bb32
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2022-12-23 10:34:26 +1300

    refactor login.php

--- classes/login.class.php
+++ classes/login.class.php
@@ -14,7 +14,7 @@ class Login extends Dbh {
 
         if($stmt->rowCount() == 0) {
             $stmt = null;
-            header("location: ../index.php?error=usernotfound");
+            header("location: ../login.php?error=usernotfound");
             exit();
         }
 
@@ -37,7 +37,7 @@ class Login extends Dbh {
 
             if($stmt->rowCount() == 0) {
                 $stmt = null;
-                header("location: ../index.php?error=usernotfound");
+                header("location: ../login.php?error=usernotfound");
                 exit();
             }
 
--- login.php
+++ login.php
@@ -1,9 +1,6 @@
 <?php
     include_once 'partials/header.php';
 
-    $uid = $pwd = '';
-    $errors = array('uid' => '', 'pwd' => '');
-
     if(isset($_GET["error"])) {
         if($_GET["error"] == "emptyinput") {
             $errors['uid'] = 'Fill in all fields!';
@@ -11,31 +8,23 @@
         else if ($_GET["error"] == "wrongpassword") {
             $errors['pwd'] = 'Incorrect login information';
         }
+        else if ($_GET["error"] == "usernotfound") {
+            $errors['uid'] = 'User not found';
+        }
     }
 ?>
-
      
     <div class="index-login-login">
         <h4>LOGIN</h4>
         <p>Don't have an account yet? Sign up here!</p>
         <form action="includes/login.inc.php" method="post">
-            <input type="text" name="uid" placeholder="Username/Email...">
-            <div class="red-text"><?php echo $errors['uid']; ?></div>
-            <input type="password" name="pwd" placeholder="Password">
-            <div class="red-text"><?php echo $errors['pwd']; ?></div>
+            <input type="text" name="uid" value="<?php echo htmlspecialchars($uid ?? '') ?>"placeholder="Username/Email...">
+            <div class="red-text"><?php echo $errors['uid'] ?? ''; ?></div>
+            <input type="password" name="pwd" value="<?php echo htmlspecialchars($pwd ?? '') ?>"placeholder="Password">
+            <div class="red-text"><?php echo $errors['pwd'] ?? ''; ?></div>
             <br>
             <button type="submit" name="submit">LOGIN</button>
-        </form>
-        <?php
-        // if(isset($_GET["error"])) {
-        //     if($_GET["error"] == "emptyinput") {
-        //         echo "<p>Fill in all fields!</p>";
-        //     }
-        //     else if ($_GET["error"] == "wrongpassword") {
-        //         echo "<p>Incorrect login information</p>";
-        //     }
-        // }
-    ?>        
+        </form>  
     </div>
 <?php
     include_once 'partials/footer.php';
